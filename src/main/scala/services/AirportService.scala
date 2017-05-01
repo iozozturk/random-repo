bfc00826824e5bc65e538361e27a440e2b04088a
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-05-01 12:16:10 +0300

    Service methods for parsing airport, country and runway data

--- src/main/scala/services/AirportService.scala
+++ src/main/scala/services/AirportService.scala
@@ -5,11 +5,16 @@ import play.api.libs.json.Json
 
 import scala.io.Source
 
-
 object AirportService {
 
-  def parseAirportsToJson() = {
-    val reader = CSVReader.open(Source.fromResource("airports.csv"))
+  def parseAirportsToJson() = parseFileToJson("airports")
+
+  def parseCountriesToJson() = parseFileToJson("countries")
+
+  def parseRunwaysToJson() = parseFileToJson("runways")
+
+  private def parseFileToJson(resourceName: String) = {
+    val reader = CSVReader.open(Source.fromResource(s"$resourceName.csv"))
     val values = reader.allWithHeaders()
     reader.close()
     values.map(Json.toJson(_))
