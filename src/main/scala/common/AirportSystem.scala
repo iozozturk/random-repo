1977dd442541e2c2caf03c82d9e0eb6f5ec8e71d
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-05-07 13:38:35 +0300

    Move elastic config and client to AirportSystem for common access

--- src/main/scala/common/AirportSystem.scala
+++ src/main/scala/common/AirportSystem.scala
@@ -1,8 +1,14 @@
 package common
 
+import java.net.InetAddress
+
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.transport.client.PreBuiltTransportClient
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -10,5 +16,14 @@ trait AirportSystem {
   implicit val system: ActorSystem = ActorSystem("airport-system")
   implicit val executor: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  private val config: Config = ConfigFactory.load()
+  private val host: String = config.getString("es.host")
+  val indexName: String = config.getString("es.index.name")
+
+  val client = new PreBuiltTransportClient(Settings.EMPTY)
+    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), 9300))
+
   val logger: LoggingAdapter
+
 }
