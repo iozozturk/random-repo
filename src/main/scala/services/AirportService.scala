7badc3926a0b81acd0bfdf78886b6665a94f79a2
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-05-07 13:37:57 +0300

    Refactor on Airport Service to parse any content type

--- src/main/scala/services/AirportService.scala
+++ src/main/scala/services/AirportService.scala
@@ -1,20 +1,16 @@
 package services
 
 import com.github.tototoshi.csv.CSVReader
+import com.google.inject.Singleton
 import play.api.libs.json.{JsValue, Json}
 
 import scala.io.Source
 
-object AirportService {
+@Singleton
+class AirportService {
 
-  def parseAirportsToJson: List[JsValue] = parseFileToJson("airports")
-
-  def parseCountriesToJson: List[JsValue] = parseFileToJson("countries")
-
-  def parseRunwaysToJson: List[JsValue] = parseFileToJson("runways")
-
-  private def parseFileToJson(resourceName: String) = {
-    val reader = CSVReader.open(Source.fromResource(s"$resourceName.csv"))
+  def parseRecordsToJson(recordType: String): List[JsValue] = {
+    val reader = CSVReader.open(Source.fromResource(s"$recordType.csv"))
     val values = reader.allWithHeaders()
     reader.close()
     values.map(Json.toJson(_))
--- src/test/scala/services/AirportServiceTest.scala
+++ src/test/scala/services/AirportServiceTest.scala
@@ -4,24 +4,26 @@ import org.scalatest.{Matchers, WordSpec}
 
 class AirportServiceTest extends WordSpec  with Matchers{
 
+  val airportService  = new AirportService
+
   "AirportServiceTest" should {
 
     "parse airport data toJson" in {
-      val airportsToJson = AirportService.parseAirportsToJson
+      val airportsToJson = airportService.parseRecordsToJson("airports")
       (airportsToJson.head \ "id").as[String] shouldEqual "1"
       (airportsToJson.head \ "ident").as[String] shouldEqual "0A"
       (airportsToJson.head \ "type").as[String] shouldEqual "heliport"
     }
 
     "parse country data toJson" in {
-      val airportsToJson = AirportService.parseCountriesToJson
+      val airportsToJson = airportService.parseRecordsToJson("countries")
       (airportsToJson.head \ "id").as[String] shouldEqual "1"
       (airportsToJson.head \ "code").as[String] shouldEqual "TR"
       (airportsToJson.head \ "name").as[String] shouldEqual "Turkey"
     }
 
     "parse runway data toJson" in {
-      val airportsToJson = AirportService.parseRunwaysToJson
+      val airportsToJson = airportService.parseRecordsToJson("runways")
       (airportsToJson.head \ "id").as[String] shouldEqual "1"
       (airportsToJson.head \ "airport_ref").as[String] shouldEqual ""
       (airportsToJson.head \ "airport_ident").as[String] shouldEqual "three"
