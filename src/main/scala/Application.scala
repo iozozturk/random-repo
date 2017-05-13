2a1dd81e3fc5f828855bc49c8608bbddc2df039b
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-05-13 13:03:31 +0300

    Index airport data at application startup

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -2,10 +2,10 @@ import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import com.google.inject.Guice
 import common.AirportSystem
-
+import httpservices.AirportHttpService
 import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 import net.codingwell.scalaguice.ScalaModule
-import httpservices.AirportHttpService
+import services.{ElasticService, IndexService}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationLong
@@ -15,8 +15,13 @@ object Application extends AirportSystem {
   def main(args: Array[String]): Unit = {
 
     val injector = Guice.createInjector()
-    val movieService = injector.instance[AirportHttpService]
-    val route = movieService.route
+    val airportHttpService = injector.instance[AirportHttpService]
+    val esService = injector.instance[ElasticService]
+    val indexService = injector.instance[IndexService]
+    val route = airportHttpService.route
+
+    esService.checkAndCreateIndex()
+    indexService.indexAllData
 
     val binding = Await.result(Http().bindAndHandle(route, "0.0.0.0", 9000), 3.seconds)
 
