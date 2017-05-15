192308833426f8955c1b6d627ccd8752b971f15b
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-05-15 16:07:45 +0300

    Move constant/singleton attributes to object container

--- src/main/scala/common/AirportSystem.scala
+++ src/main/scala/common/AirportSystem.scala
@@ -6,6 +6,7 @@ import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.stream.ActorMaterializer
 import com.typesafe.config.{Config, ConfigFactory}
+import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.transport.client.PreBuiltTransportClient
@@ -17,15 +18,18 @@ trait AirportSystem {
   implicit val executor: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
+  val maxResultValue = 10000
+  val client: TransportClient = Constants.client
+  val indexName: String = Constants.indexName
+
+  val logger: LoggingAdapter
+}
+
+object Constants {
   private val config: Config = ConfigFactory.load()
   private val host: String = config.getString("es.host")
   val indexName: String = config.getString("es.index.name")
 
-  val maxResultValue = 10000
   val client = new PreBuiltTransportClient(Settings.EMPTY)
     .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), 9300))
-
-
-  val logger: LoggingAdapter
-
 }
